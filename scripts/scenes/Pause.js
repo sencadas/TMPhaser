5be1bd14e7b853c849f4e86ba26562ec2059c133
João Sencadas <joao.sencadas.tgp2@gmail.com>
2022-05-08 15:37:55 +0100

    Pause created

---
+++ scripts/scenes/Pause.js
@@ -0,0 +1,32 @@
+export class Pause extends Phaser.Scene {
+  constructor() {
+    super({ key: "pause" });
+  }
+
+  create() {
+    var title = this.add
+      .bitmapText(20, 200, "arcade", "Pause")
+      .setTint(0xff00ff);
+
+    this.tweens.add({
+      targets: title,
+      y: 220,
+      duration: 5000,
+      yoyo: true,
+      repeat: -1,
+    });
+
+    var bg = this.add.image(400, 400, "playbutton");
+    bg.setOrigin(0.5, 0.5).setDisplaySize(200, 200);
+
+    bg.setInteractive();
+
+    bg.once(
+      "pointerup",
+      function () {
+        this.scene.start("game");
+      },
+      this
+    );
+  }
+}
