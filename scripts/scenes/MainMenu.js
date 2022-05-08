e3adf4211120a333c92141342fa1cbf31c8c1dd8
Ruben Morim <rubenmorim17@gmail.com>
2022-05-08 15:24:23 +0100

    new menu

--- scripts/scenes/MainMenu.js
+++ scripts/scenes/MainMenu.js
@@ -1,3 +1,6 @@
+let height = window.innerHeight;
+let width = window.innerWidth;
+
 export class MainMenu extends Phaser.Scene {
   constructor() {
     super({ key: "mainmenu" });
@@ -5,18 +8,37 @@ export class MainMenu extends Phaser.Scene {
 
   create() {
     var title = this.add
-      .bitmapText(20, 200, "arcade", "Welcome to Space Defense")
+      .bitmapText(width / 4.3, 100, "arcade", "Welcome to Space Defense")
       .setTint(0xff00ff);
 
+    var madeBy = this.add
+      .bitmapText(
+        width / 3,
+        500,
+        "arcade",
+        "Made By: Joao Sencadas N_20381 & Ruben Morim N_20399",
+        10
+      )
+      .setTint(0xff00ff);
+
+    //Efeito no texto
     this.tweens.add({
       targets: title,
-      y: 220,
+      y: 150,
       duration: 5000,
       yoyo: true,
       repeat: -1,
     });
 
-    var bg = this.add.image(400, 400, "playbutton");
+    this.tweens.add({
+      targets: madeBy,
+      y: 550,
+      duration: 5000,
+      yoyo: true,
+      repeat: -1,
+    });
+
+    var bg = this.add.image(width / 2, height / 2, "playbutton");
     bg.setOrigin(0.5, 0.5).setDisplaySize(200, 200);
 
     bg.setInteractive();
